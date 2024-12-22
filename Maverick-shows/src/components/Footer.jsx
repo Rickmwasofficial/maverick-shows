3a848702f899b0c13f188492ada957b0cff364dd
Rickmwasofficial <rickmwasofficial@gmail.com>
2024-12-22 19:29:34 +0300

    footer

--- Maverick-shows/src/components/Footer.jsx
+++ Maverick-shows/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
     return (
-        <footer className="px-4 divide-y dark:bg-gray-100 dark:text-gray-800">
-            <div className="py-6 text-sm text-center dark:text-gray-600">© 2024 by Rickmwasofficial. All rights reserved.</div>
+        <footer className="px-4 divide-y bg-black">
+            <div className="py-6 text-sm text-center text-white-600">© 2024 by Rickmwasofficial. All rights reserved.</div>
         </footer>
     )
 }
